99baf9e6b4a62793fdf75c8c22cca5c55bd41c09
Vladimir <48253536+vovasazonov@users.noreply.github.com>
2021-01-15 15:13:58 +0300

    add: test DialogManager

--- DialogManagerProject/Packages/dialog-manager/Tests/Runtime/DialogManagerTest.cs
+++ DialogManagerProject/Packages/dialog-manager/Tests/Runtime/DialogManagerTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using NUnit.Framework;
 using Dialogs;
 using NSubstitute;
@@ -9,13 +10,55 @@ namespace Tests.Runtime
     public class DialogManagerTest
     {
         [Test]
-        public void CloseLastDialog_ThereIsNoDialogs_DoesNotThrow()
+        public void CloseLastDialog_ThereIsNoDialogs_DoesNotThrowException()
         {
-            var customDialogDic = Substitute.For<IDictionary<string, IDialog>>();
-            var commonDialogDic = Substitute.For<IDictionary<CommonDialogType, ICommonDialog>>();
+            var customDialogDic = new Dictionary<string, IDialog>();
+            var commonDialogDic = new Dictionary<CommonDialogType, ICommonDialog>();
             var dialogManager = new DialogManager(customDialogDic, commonDialogDic);
-            
-            Assert.DoesNotThrow(()=>dialogManager.CloseLastDialog());
+
+            Assert.DoesNotThrow(() => dialogManager.CloseLastDialog());
+        }
+
+        [Test]
+        public void OpenDialog_ThereIsNoSuchDialog_ThrowException()
+        {
+            var customDialogDic = new Dictionary<string, IDialog>();
+            var commonDialogDic = new Dictionary<CommonDialogType, ICommonDialog>();
+            var dialogManager = new DialogManager(customDialogDic, commonDialogDic);
+
+            Assert.Throws<KeyNotFoundException>(() => dialogManager.OpenDialog(""));
+        }
+
+        [Test]
+        public void OpenDialog_ThereIsSuchDialog_DoesNotThrowException()
+        {
+            var customDialogDic = new Dictionary<string, IDialog> {{"", Substitute.For<IDialog>()}};
+            var commonDialogDic = new Dictionary<CommonDialogType, ICommonDialog>();
+            var dialogManager = new DialogManager(customDialogDic, commonDialogDic);
+
+            Assert.DoesNotThrow(() => dialogManager.OpenDialog(""));
+        }
+
+        [Test]
+        public void OpenCommonDialog_ThereIsNoSuchDialog_ThrowException()
+        {
+            var customDialogDic = new Dictionary<string, IDialog>();
+            var commonDialogDic = new Dictionary<CommonDialogType, ICommonDialog>();
+            var buttonActions = new Dictionary<ButtonDialogType, Action>();
+            var dialogManager = new DialogManager(customDialogDic, commonDialogDic);
+
+            Assert.Throws<KeyNotFoundException>(() => dialogManager.OpenCommonDialog("", "", CommonDialogType.OkCancel, buttonActions));
+        }
+        
+        [Test]
+        public void OpenCommonDialog_ThereIsSuchDialog_DoesNotThrowException()
+        {
+            var customDialogDic = new Dictionary<string, IDialog>();
+            var commonDialogDic = new Dictionary<CommonDialogType, ICommonDialog> {{CommonDialogType.OkCancel, Substitute.For<ICommonDialog>()}};
+            var buttonActions = new Dictionary<ButtonDialogType, Action>();
+            var dialogManager = new DialogManager(customDialogDic, commonDialogDic);
+
+            Assert.DoesNotThrow(() => dialogManager.OpenCommonDialog("", "", CommonDialogType.OkCancel, buttonActions));
         }
     }
 }
